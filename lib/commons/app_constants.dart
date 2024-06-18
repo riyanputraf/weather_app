a180799ea803f69914abdc09e70411066b82a703
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-06-18 14:18:11 +0700

    feat: create app constant
    Create app constant for image

---
+++ lib/commons/app_constants.dart
@@ -0,0 +1,12 @@
+const weatherBg = {
+  'clear sky': 'assets/clear_sky.jpg',
+  'few clouds': 'assets/few_clouds.jpg',
+  'scattered clouds': 'assets/scattered_clouds.jpg',
+  'broken clouds': 'assets/broken_clouds.jpg',
+  'overcast clouds': 'assets/overcast_clouds.jpg',
+  'shower rain': 'assets/shower_rain.jpg',
+  'rain': 'assets/rain.jpg',
+  'thunderstorm': 'assets/thunderstorm.jpg',
+  'snow': 'assets/snow.jpg',
+  'mist': 'assets/mist.jpg'
+};
